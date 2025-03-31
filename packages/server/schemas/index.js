87e6f9dafa313993f919de841d404411b10a099f
seonyeonghun2 <kjca1117@naver.com>
2025-03-31 10:10:13 +0900

    mongodb 연결 설정 | mongoose 패키지 활용
    1. development : debug 설정
    
    2. mongoose.connection : 연결 해제시 자동으로 재연결 시도

--- packages/server/schemas/index.js
+++ packages/server/schemas/index.js
@@ -1,19 +1,21 @@
-// const mongoose = require('mongoose');
 import mongoose from 'mongoose'
+import 'dotenv/config'
 
-async function connect(uri) {
-  try {
-    await mongoose.connect(uri);
-  } catch(e) {
-    console.log(e)
+const connect = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    mongoose.set('debug', true)
   }
+  mongoose.connect(process.env.DB_URI)
+  .then(() => console.log("Mongodb is connected!"))
+  .catch((err) => console.log(err))
 }
-export default connect;
 
-// mongoose.on('error', () => {
-//     console.log('Mongodb Connect is failed!');
-// });
-// mongoose.on('disconnected', () => {
-//     console.log('Connect is disconnected!');
-//     connect(); // 연결 재시도
-// })
+mongoose.connection.on('error', () => {
+    console.log('Mongodb Connect is failed!');
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Connect is disconnected!');
+    connect(); // 연결 재시도
+})
+
+export default connect;
