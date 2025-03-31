cbc4c7fb4f8aaa1ef4e9055498807b780924f968
seonyeonghun2 <kjca1117@naver.com>
2025-03-31 12:32:49 +0900

    post 요청 처리
    1. 이미 존재하는 user 확인
    
    2. 없으면 신규 user 저장(=생성

--- packages/server/controllers/regist.contoller.js
+++ packages/server/controllers/regist.contoller.js
@@ -1,10 +1,25 @@
-const regist = (req, res) => {
+import User from "../models/User.js";
+const regist = async (req, res) => {
   const {username, passwd, email, phone, sns} = req.body;
-  console.log(req.body);
-  // res.json({
-  //   message: "user is created!",
-  //   data: req.body
-  // })
+  try {    
+    // 이미 등록된 회원이 있는지 : _id (몽고db가 자체적으로?)
+    const existUser = await User.findOne({
+      $or: [{email}, {username}, {phone}]
+    })
+
+    if (existUser) {
+      return res.status(400).json({
+        message: '해당 정보로 등록된 사용자가 이미 존재합니다.'
+      })
+    }
+    
+    // 아니라면(=없다면) 새 회원을 생성(=등록)
+    const user = new User(req.body);
+    await user.save()
+  } catch (err) {
+    console.log("resit is failed : ", err)
+  }
+  
 }
 
 export default regist
