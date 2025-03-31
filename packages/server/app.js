00b1bc9c34764b5681d246dd855c199150a51577
seonyeonghun2 <kjca1117@naver.com>
2025-03-31 10:08:05 +0900

    DB_URI | 환경변수 문자열, 모듈 삭제

--- packages/server/app.js
+++ packages/server/app.js
@@ -6,7 +6,7 @@ import connect from './schemas/index.js';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.route.js'
 
-connect(process.env.DB_URI);
+connect();
 
 app.use('/', indexRouter);
 app.use('/users', userRouter); // path가 /user 라고 요청오면 처리(CRUD)
