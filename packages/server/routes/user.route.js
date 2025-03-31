98195dd71f4aabc2f8c291ee890bda37869a36d4
seonyeonghun2 <kjca1117@naver.com>
2025-03-31 11:05:21 +0900

    user 라우터 | post 요청 분기
    1. controllers/regist 컨트롤러

--- packages/server/routes/user.route.js
+++ packages/server/routes/user.route.js
@@ -1,18 +1,16 @@
 import express from 'express'
 const router = express.Router()
+import regist from '../controllers/regist.contoller.js'
 
-
-  router.get('/', (req, res) => {
-    res.send('users list is returned!')
-  })
-  router.post('/', (req, res) => {
-    res.send('new user is created!')
-  })
-  router.put('/:id', (req, res) => {
-    res.send('some user is updated!')
-  })
-  router.delete('/:id', (req, res) => {
-    res.send('some user is removed!')
-  })
+router.get('/', (req, res) => {
+  res.send('users list is returned!')
+})
+router.post('/', regist)
+router.put('/:id', (req, res) => {
+  res.send('some user is updated!')
+})
+router.delete('/:id', (req, res) => {
+  res.send('some user is removed!')
+})
 
 export default router;
