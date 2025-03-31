38a8218a9e06c6a54c5a67c2fad6cc37931af3d6
seonyeonghun2 <kjca1117@naver.com>
2025-03-31 11:02:02 +0900

    express 미들웨어 | 추가(=장착)
    1. json 미들웨어
    2. multipart/formdata 미들웨어

--- packages/server/app.js
+++ packages/server/app.js
@@ -7,7 +7,8 @@ import indexRouter from './routes/index.js';
 import userRouter from './routes/user.route.js'
 
 connect();
-
+app.use(express.json()); // json 해석기
+app.use(express.urlencoded({extended: true})); // form데이터 해석기
 app.use('/', indexRouter);
 app.use('/users', userRouter); // path가 /user 라고 요청오면 처리(CRUD)
 export default app;
