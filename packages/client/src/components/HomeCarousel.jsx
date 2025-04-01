0ab143125045b5cdef0091e7dd49ea52b407b652
seonyeonghun2 <kjca1117@naver.com>
2025-04-01 13:06:31 +0900

    리액트 부트스트랩 | 캐러셀 컴포넌트
    1. 홈 스크린 이미지  : 반복 재사용 (임시)
    
    2. 나머지 이미지는 추후에 변경예정

---
+++ packages/client/src/components/HomeCarousel.jsx
@@ -0,0 +1,36 @@
+import { Container, Carousel } from 'react-bootstrap'
+import HomeImage from '../assets/home-section.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function HomeCarousel() {
+  return (
+    <Container className='my-3'>
+      <Carousel>
+        <Carousel.Item>
+          <img src={HomeImage} alt="home" className='img-fluid' />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={HomeImage} alt="home" className='img-fluid' />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={HomeImage} alt="home" className='img-fluid' />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
+}
+
+export default HomeCarousel;
