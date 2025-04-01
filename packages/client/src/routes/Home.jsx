a52809382258d3663c0e109b345d6101a77a7392
seonyeonghun2 <kjca1117@naver.com>
2025-04-01 13:05:16 +0900

    라우트 컴포넌트 생성

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import SiteHeader from '../layouts/SiteHeader'
+import SiteFooter from '../layouts/SiteFooter'
 
+import HomeCarousel from '../components/HomeCarousel'
+
+import { Outlet } from 'react-router'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <SiteHeader />
+      <HomeCarousel />
+      <Outlet />
+      <SiteFooter />
+    </>
   )
 }
 
