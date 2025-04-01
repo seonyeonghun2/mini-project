22ffe32541ed3eff61a7361ed405fd255f7ef1d7
seonyeonghun2 <kjca1117@naver.com>
2025-04-01 13:03:56 +0900

    페이지 라우팅
    1. react-router 를 이용한 url 경로 설정
    
    2. 라우트 컴포넌트 생성 및 호출 : HOME ~ Board
    
    3. 중첩 라우팅 구현

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Routes, Route} from 'react-router'
 import Home from './routes/Home'
+import About from './routes/About'
+import SignIn from './routes/SignIn'
 import SignUp from './routes/SignUp'
+import Board from './routes/Board'
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/' element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path='signin' element={<SignIn />} />
+          <Route path='board' element={<Board />} />
+        </Route>
       </Routes>
     </>
   )
