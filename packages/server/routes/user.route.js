d41cd9cd505856ac17a80e8759d60202cd81367c
seonyeonghun2 <kjca1117@naver.com>
2025-04-03 12:33:50 +0900

    user 라우터 변경
    1. regist.controller.js -> user.controller.js : 이름 변경에 따라서 userControl로 변경
    
    2.로그인 라우터 작성

--- packages/server/routes/user.route.js
+++ packages/server/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import regist from '../controllers/regist.contoller.js'
+import userControl from '../controllers/user.contoller.js'
+
 
 router.get('/', (req, res) => {
   res.send('users list is returned!')
 })
-router.post('/', regist)
+router.post('/regist', userControl.regist)
+router.post('/login', userControl.login)
 router.put('/:id', (req, res) => {
   res.send('some user is updated!')
 })
