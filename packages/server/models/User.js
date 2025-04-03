fec24fe9b5ff62ae42c759591318b3733758019f
seonyeonghun2 <kjca1117@naver.com>
2025-04-03 12:32:54 +0900

    User 스키마 변경
    1. passwd -> password : 필드명 변경
    
    2. mongoose 미들웨어 추가 : pre Hooks
    
    3. bcrypt : 해싱처리(암호화 알고리즘 적용)

--- packages/server/models/User.js
+++ packages/server/models/User.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
-
+import bcrypt from 'bcrypt'
 const userSchema = mongoose.Schema({
     username: {
         type: String,
         required: true        
     },
-    passwd: {
+    password: {
         type: String,
-        required: true,
+        required: true
     },
     email: {
         type: String,
@@ -19,6 +19,16 @@ const userSchema = mongoose.Schema({
     },
     sns: String
 })
+
+userSchema.pre('save', async function(next) {
+    // do stuff
+    try {
+        const salt = await bcrypt.genSalt(10); // 기본 10, 20, 30,...
+        this.password = await bcrypt.hash(this.password, salt); // $2b$10$0MKjRJK74KAe4K8rYirQbuucLWhHV2zhTKjDXriJUsuU6YsPrr9ca
+    } catch (err) {
+        next(err)
+    }    
+  });
 
 const User = mongoose.model('User', userSchema);
 
