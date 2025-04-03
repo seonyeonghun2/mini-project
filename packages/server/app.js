1d290271ffbd488d261715b83ff263158c51e18b
seonyeonghun2 <kjca1117@naver.com>
2025-04-03 12:30:51 +0900

    패키지 추가
    1. mogran : 백엔드 로그용
    2. cors : 로컬 개발시 cors 오류 해결용
    3. 미들웨어 장착 : morgan, cors

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,12 +1,15 @@
 // const express = require('express')
 import express from "express"
-
 const app = express()
+import logger from 'morgan'
+import cors from 'cors'
 import connect from './schemas/index.js';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.route.js'
 
 connect();
+app.use(logger('dev'))
+app.use(cors())
 app.use(express.json()); // json 해석기
 app.use(express.urlencoded({extended: true})); // form데이터 해석기
 app.use('/', indexRouter);
