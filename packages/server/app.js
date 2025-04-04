8289f54536b838217895af4ed72bb0afa8f2d18b
seonyeonghun2 <kjca1117@naver.com>
2025-04-04 13:09:50 +0900

    백엔드 | express 설정
    1. cors 옵션 추가 (credentials: true)
    
    서로 다른 도메인 (:3000 vs :5173)간 쿠키 전송을 허용
    
    2. cookieParser 추가

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,6 +2,7 @@
 import express from "express"
 const app = express()
 import logger from 'morgan'
+import cookieParser from "cookie-parser"
 import cors from 'cors'
 import connect from './schemas/index.js';
 import indexRouter from './routes/index.js';
@@ -9,9 +10,14 @@ import userRouter from './routes/user.route.js'
 
 connect();
 app.use(logger('dev'))
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json()); // json 해석기
 app.use(express.urlencoded({extended: true})); // form데이터 해석기
 app.use('/', indexRouter);
 app.use('/users', userRouter); // path가 /user 라고 요청오면 처리(CRUD)
+app.use('/posts', postRouter)
 export default app;
