05c25d4e58487e3084c11a35e2952e79cbee3a46
seonyeonghun2 <kjca1117@naver.com>
2025-04-04 13:08:14 +0900

    로그인 유지를 위한 state, prop 설정
    1. js-cookie 패키지 : 쿠키 정보를 파악, 해석
    
    2. userInfo 스테이트에 로그인 정보를 저장, props를 통해 SIteHeader.jsx 전달
    
    3. 로그아웃용 함수 정의, props로 SIteHeader.jsx에 전달

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
 import SiteHeader from '../layouts/SiteHeader'
 import SiteFooter from '../layouts/SiteFooter'
-
 import HomeCarousel from '../components/HomeCarousel'
-
 import { Outlet } from 'react-router'
 const Home = () => {
+  const [userInfo, setUserInfo] = useState(null)
+  useEffect(() => {
+    const cookie = Cookies.get('nexcent')
+    // console.log(cookie.split('\"')[3]) //관리자(username)
+    if(cookie) {
+      setUserInfo(cookie.split('\"')[3])
+    }
+  }, [])
+  const handleLogOut = () => {
+    if(confirm("로그아웃 하시겠습니까?")) {
+      Cookies.remove('nexcent')
+      location.href='/'
+    }
+  }
   return (
     <>
-      <SiteHeader />
+      <SiteHeader userInfo={userInfo} handleLogOut={handleLogOut} />
       <HomeCarousel />
       <Outlet />
       <SiteFooter />
