35df4fe4f94a2940d2a2b85bf739e436bc9ac1a1
seonyeonghun2 <kjca1117@naver.com>
2025-04-04 13:07:08 +0900

    로그인 유지 처리
    1. 내비게이션 변경 : 로그인/로그아웃 + 메인 메뉴 UI 분리
    
    2. 쿠키 유무에 따른 로그인/로그아웃 처리
    
    3. 쿠키 삭제 로그아웃

--- packages/client/src/layouts/SiteHeader.jsx
+++ packages/client/src/layouts/SiteHeader.jsx
@@ -4,27 +4,39 @@ import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import { Container, Row, Col, Button} from 'react-bootstrap'
 import siteLogo from '../assets/site-logo.png'
 import { NavLink } from 'react-router'
-const SiteHeader = () => {
+const SiteHeader = ({ userInfo, handleLogOut }) => {
     const handleClick = () => {
         location.href='/signup'
     }
   return (
     <header className='py-3'>
         <Container>
-            <Row className='align-items-center justify-content-between'>
-                <Col className='logo'>
+            <div className='w-100 text-end border-bottom py-2'>{ userInfo ? (
+                <>
+                    <span>{userInfo}님 로그인중</span> {" | "}
+                    <NavLink to='/modify'>정보수정</NavLink> {" "}
+                    <NavLink onClick={handleLogOut}>로그아웃</NavLink>
+                </>
+            ) : (
+                <>
+                    <NavLink to='/signin'>로그인</NavLink>{" "}
+                    <Button onClick={handleClick}>Register Now <i className="bi bi-arrow-right"></i></Button>
+                </>
+            )}</div>
+        </Container>
+        <Container>
+            <Row className='align-items-center justify-content-between my-3'>
+                <Col className='logo' md="4">
                     <img src={siteLogo} alt="logo" />
                 </Col>
-                <Col className="d-flex align-items-center gap-3 ml-auto">
-                    <nav>
-                        <ul className="d-flex gap-3">
+                <Col className="d-flex align-items-center gap-3" md="8">
+                    <nav className='w-100'>
+                        <ul className="d-flex gap-5 justify-content-between">
                             <li><NavLink to='/'>Home</NavLink></li>
                             <li><NavLink to='/about'>about</NavLink></li>
-                            <li><NavLink to='/board'>community</NavLink></li>
-                            <li><NavLink to='/signin'>로그인</NavLink></li>
+                            <li><NavLink to='/board'>board</NavLink></li>
                         </ul>
                     </nav>
-                    <Button onClick={handleClick}>Register Now <i className="bi bi-arrow-right"></i></Button>
                 </Col>
             </Row>
         </Container>
