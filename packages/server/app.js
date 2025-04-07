4f7139888a4486c574e19755775f356acdb7f29d
seonyeonghun2 <kjca1117@naver.com>
2025-04-07 18:06:44 +0900

    posts 라우터 | 미들웨어 등록

--- packages/server/app.js
+++ packages/server/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 import connect from './schemas/index.js';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.route.js'
-
+import postRouter from './routes/post.route.js'
 connect();
 app.use(logger('dev'))
 app.use(cookieParser())
@@ -19,5 +19,5 @@ app.use(express.json()); // json 해석기
 app.use(express.urlencoded({extended: true})); // form데이터 해석기
 app.use('/', indexRouter);
 app.use('/users', userRouter); // path가 /user 라고 요청오면 처리(CRUD)
-app.use('/posts', postRouter)
+app.use('/api', postRouter)
 export default app;
