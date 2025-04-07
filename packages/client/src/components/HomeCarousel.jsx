ed93bc2bafdf77e7d6dfe24b79a47acb314cc5ed
seonyeonghun2 <kjca1117@naver.com>
2025-04-07 18:04:36 +0900

    캐러셀 UI 변경
    1. width 100% 적용
    
    2. container fluid 적용

--- packages/client/src/components/HomeCarousel.jsx
+++ packages/client/src/components/HomeCarousel.jsx
@@ -3,24 +3,24 @@ import HomeImage from '../assets/home-section.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function HomeCarousel() {
   return (
-    <Container className='my-3'>
+    <Container className='my-3 px-0' fluid>
       <Carousel>
         <Carousel.Item>
-          <img src={HomeImage} alt="home" className='img-fluid' />
+          <img src={HomeImage} alt="home" className='w-100 img-fluid' />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt="home" className='img-fluid' />
+        <img src={HomeImage} alt="home" className='w-100 img-fluid' />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt="home" className='img-fluid' />
+        <img src={HomeImage} alt="home" className='w-100 img-fluid' />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
