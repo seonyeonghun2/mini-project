28dd385fdf785e52f640813a295bd7abc75144b9
seonyeonghun2 <kjca1117@naver.com>
2025-04-08 16:38:24 +0900

    리액트 부트스트랩 Form 적용
    1. 오류 : post.author.username

--- packages/client/src/routes/ViewPost.jsx
+++ packages/client/src/routes/ViewPost.jsx
@@ -1,92 +1,176 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Cookies from 'js-cookie'
-import { useParams, useNavigate } from 'react-router';
-import { Container, Row, Col, Button }from 'react-bootstrap';
+import { useParams, useNavigate } from 'react-router'
+import { Container, Row, Col, Button, Form, InputGroup } from 'react-bootstrap'
 
 function ViewPost() {
-    const [post, setPost] = useState(null)
-    const [loading, setLoading] = useState(true)
-    let navigate = useNavigate();
-    let params = useParams();
+  let navigate = useNavigate()
+  let uuid = useParams()
+  const [post, setPost] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [validated, setValidated] = useState(false)
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+    createdAt: ''
+  })
+  const token = Cookies.get('nexcent')
+  if (!token) {
+    alert('인증 정보가 존재하지 않습니다')
+    navigate('/signin')
+  }
 
-    const token = Cookies.get('nexcent')
-    if (!token) {
-        alert("인증 정보가 존재하지 않습니다");
-        navigate('/signin')        
+  useEffect(() => {
+    console.log(uuid.id)
+    axios
+      .get(`http://localhost:3000/api/posts/${uuid.id}`, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        // console.log(res.data)
+        setPost(res.data)
+        setLoading(false)
+      })
+      .catch((err) => console.error(err.message))
+  }, [uuid.id])
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.stopPropagation()
     }
+    setValidated(true)
+    axios
+      .put(`http://localhost:3000/api/posts/${uuid.id}`, formData, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setPost(res.data)
+        console.log(res)
+        setLoading(false)
+      })
+      .catch((err) => console.error(err.message))
+  }
 
-    useEffect(()=>{
-        axios.get(`http://localhost:3000/api/posts/${params.id}`, {
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then((res) => {
-            setPost(res.data)
-            setLoading(false)
-        }).catch((err) => console.error(err.message))
-    }, [])
+  // console.log(params); // _id가 아닌 uuid값
 
-    function convertDate(date) {       
-        return new Date(date).toLocaleDateString('ko-KR', {
-            year: '2-digit',
-            month: '2-digit',
-            day: '2-digit'
-        }).replaceAll('.','').replaceAll(' ','-')
-    }
-    function goPostsList() {
-        navigate('/posts')
-    }
-    function removePost(id){
-        axios.delete(`http://localhost:3000/api/posts/${id}`, {
-            withCredentials: true,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then((res) => {
-            if(res.status===201 || res.statusText === 'Created') {
-                navigate('/posts')
-            }
-        }).catch((err) => console.error(err.message))
-    }
-    if (loading) {
-        return <div>로딩중...</div>
-    }
-    return (
-        <>
-            <Container className='py-5'>
-                <Row>
-                    <Col>
-                        <h2 className="text-center my-3">상세보기</h2>
-                        <table className="table table-striped d-flex gap-1">
-                            <thead className="w-25">
-                                <tr className='d-flex flex-column'>
-                                    <th>제목</th>
-                                    <th>작성자</th>
-                                    <th>작성일</th>
-                                    <th className="flex-grow-10">내용</th>
-                                </tr>
-                            </thead>
-                            <tbody className="w-75">
-                                <tr className='d-flex flex-column'>
-                                    <td className="text-start">{post.title}</td>
-                                    <td className="text-start">{post.author.username}</td>
-                                    <td className="text-start">{convertDate(post.createdAt)}</td>
-                                    <td className="text-start">{post.content}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div className='d-flex gap-1 justify-content-end'>
-                            <Button variant="primary" onClick={goPostsList}>목록</Button>
-                            <Button variant="secondary">수정</Button>
-                            <Button variant="success" onClick={() => removePost(params.id)}>삭제</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  function convertDate(date) {
+    return new Date(date)
+      .toLocaleDateString('ko-KR', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .replaceAll('.', '')
+      .replaceAll(' ', '-')
+  }
+  function goPostsList() {
+    navigate('/posts')
+  }
+  function removePost(id) {
+    axios
+      .delete(`http://localhost:3000/api/posts/${id}`, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.status === 201 || res.statusText === 'Created') {
+          navigate('/posts')
+        }
+      })
+      .catch((err) => console.error(err.message))
+  }
+  if (loading) {
+    return <div>로딩중...</div>
+  }
+  return (
+    <>
+      <Container className="my-5">
+        <h1 className="text-center mb-5">상세보기</h1>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row>
+            <Col>
+              <Form.Group as={Col} controlId="validationCustomDate">
+                <Form.Label>작성일</Form.Label>
+                <Form.Control
+                  required
+                  type="date"
+                  placeholder="created at"
+                  disabled
+                  defaultValue={convertDate(post.createdAt)}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group as={Col} controlId="validationCustomAuthor">
+                <Form.Label>작성자</Form.Label>
+                <Form.Control type="text" placeholder="author" defaultValue={post.author.username} disabled required />
+                <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="my-3">
+            <Form.Group as={Col} md="12" controlId="validationCustomTitle">
+              <Form.Label>제목</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="title text"
+                name="title"
+                onChange={handleChange}
+                defaultValue={post.title}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group as={Col} md="12" controlId="validationCustomContent">
+              <Form.Label>내용</Form.Label>
+              <Form.Control
+                type="text"
+                as="textarea"
+                placeholder="author"
+                name="content"
+                onChange={handleChange}
+                defaultValue={post.content}
+                required
+              />
+              <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row className="mt-3">
+            <Col className="d-flex gap-1 justify-content-end">
+              <Button variant="primary" type="button" onClick={goPostsList}>
+                목록
+              </Button>
+              <Button variant="secondary" type="submit">
+                수정
+              </Button>
+              <Button variant="success" type="button" onClick={() => removePost(params.id)}>
+                삭제
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </>
+  )
 }
 
-export default ViewPost;
+export default ViewPost
