5cadad5b3467ea5990fa83c7ef4aaf4d9025f113
seonyeonghun2 <kjca1117@naver.com>
2025-04-08 14:12:30 +0900

    개별 글 라우팅 처리
    1. 조회 : get 요청시 readPost()
    
    2. 삭제 : delete 요청 removePost()

--- packages/server/routes/post.route.js
+++ packages/server/routes/post.route.js
@@ -3,7 +3,9 @@ import * as postController from '../controllers/post.controller.js'
 const router = express.Router()
 
 router.post('/posts', postController.createPosts) // 쓰기
-router.get('/posts', postController.readPosts) // 글 읽기
+router.get('/posts', postController.readPosts) // 전체 글 읽기
+router.get('/posts/:id', postController.readPost) // 개별 글 읽기
+router.delete('/posts/:id', postController.removePost) // 글 삭제
 /*
 router.put() // 수정
 router.delete() // 삭제
