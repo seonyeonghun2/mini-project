2e9c9360f9b119d836b9c966f8257c29b1594aea
seonyeonghun2 <kjca1117@naver.com>
2025-04-08 14:09:34 +0900

    글 목록 컴포넌트
    1. 토큰 확인 : state 관리
    
    2. 글 목록 요청 : 글 작성자 정보 (mongoose populate API)
    
    3. 글 작성일 : yy-mm-dd 형식으로 변환하는 convertDate() 정의
    
    4. 글쓰기 버튼 : token 유무를 체크하고 state로 관리 (isAuth), 글쓰기 버튼 표시
    
    5. 글쓰기 : 버튼 클릭시 /addPost 페이지로 라우팅 처리

--- packages/client/src/routes/Posts.jsx
+++ packages/client/src/routes/Posts.jsx
@@ -1,36 +1,72 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import {Container,Row,Col} from 'react-bootstrap'
+import {useNavigate, Link} from 'react-router'
 const Posts = () => {
     const [posts, setPosts] = useState(null)
-    const [loading, setLoading] = useState(true);
-
+    const [loading, setLoading] = useState(true)
+    const [isAuth, setIsAuth] = useState(false)
+    let navigate = useNavigate()
     useEffect(() => {
-        axios.get('http://localhost:3000/api/posts', {
-            withCredentials: true
-        })
-            .then(res => {
-                console.log(res)
+        const token = Cookies.get("nexcent")
+
+        token ? setIsAuth(true) : setIsAuth(false)
+
+        axios
+            .get('http://localhost:3000/api/posts', {
+                withCredentials: true,
+            })
+            .then((res) => {
+                // console.log(res)
                 setLoading(false)
                 setPosts(res.data)
-            }).catch(err => console.error(err.message))
+            })
+            .catch((err) => console.error(err.message))
     }, [])
+    function convertDate(date) {       
+        return new Date(date).toLocaleDateString('ko-KR', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit'
+        }).replaceAll('.','').replaceAll(' ','-')
+    }
+    function goPostWrite(){
+        navigate('/addPost')
+    }
     return (
-        <div className="py-5">
+        <Container className="py-5">
             {loading && <div>로딩중...</div>}
-            <table className='table table-striped w-75 mx-auto'>
-                <thead>
-                    <tr>
-                        <th>번호</th>
-                        <th>제목</th>
-                        <th>글쓴이</th>
-                        <th>작성일</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {posts && posts.map((post, i) => <tr key={i}><td>{i + 1}</td><td>{post.title}</td><td>{post.author}</td><td>{new Date(post.createdAt).toLocaleDateString()}</td></tr>)}
-                </tbody>
-            </table>
-        </div>
+            <Row>
+                <h2 className='text-center mb-3'>커뮤니티 | 글목록</h2>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>글쓴이</th>
+                            <th>작성일</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {posts &&
+                            posts.map((post, i) => (
+                                <tr key={i}>
+                                    <td>{i + 1}</td>
+                                    <td><Link to={`/post/${post._id}`}>{post.title}</Link></td>
+                                    <td>{post.author.username}</td>
+                                    <td>
+                                        {convertDate(post.createdAt)}
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </Row>
+            <Row className='justify-content-end'>
+                <Col md={2}>{isAuth && <button onClick={goPostWrite}>글쓰기</button>}</Col>
+            </Row>
+        </Container>
     )
 }
 
